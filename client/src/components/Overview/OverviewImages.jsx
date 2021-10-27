17167c8465dd6fdacec9ce3bb4140101cc2195fa
Clayton Watterson <cwatterson@gmail.com>
2021-10-27 00:09:57 -0400

    Add basic skeleton for Overview Component

---
+++ client/src/components/Overview/OverviewImages.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class OverviewImages extends React.Component {
+
+  // Component for image gallery of current product
+
+  render() {
+    return (
+    <img src='https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-product-2_large.png?format=jpg&quality=90&v=1530129318'></img>
+    );
+  }
+}
+
+export default OverviewImages;
---
+++ client/src/components/Overview/OverviewStyles.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class OverviewStyles extends React.Component {
+
+  // Component for displaying and selecting styles of product
+
+  render() {
+    return (
+      <div>
+        STYLE PICKER
+      </div>
+    );
+  }
+}
+
+export default OverviewStyles;
