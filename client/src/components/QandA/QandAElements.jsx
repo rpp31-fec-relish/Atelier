8220830aebe6bf7dcbc110e61d3f114a5ca84cfd
Juan Acosta <jacosta091@gmail.com>
2021-10-26 17:59:08 -0700

    Completed making skeleton

---
+++ client/src/components/QandA/QandAElements.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class QandAElements extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div> QandA Element </div>
+    );
+  }
+
+}
+
+export default QandAElements;
--- client/src/components/QandA/main.jsx
+++ client/src/components/QandA/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import QandAElements from './QandAElements.jsx';
 
 class QandA extends React.Component {
 
@@ -7,7 +8,20 @@ class QandA extends React.Component {
   }
 
   render() {
-    return (<h2>QandA Here</h2>);
+    return (
+      <div id='QandAContainer'>
+        <div id='QandATitle'>Questions & Answers</div>
+        <input type='text' id='QandASearchBar'placeholder='HAVE A QUESTION? SEARCH FOR ANSWERS...'></input>
+        <QandAElements/>
+        <div id='MoreAnswersContainer'>
+        <button id='MoreAnswers'> LOAD MORE ANSWERS </button>
+        </div>
+        <div id='AskMoreQuestionsContainer'>
+          <button id='MoreAnswerdQuestionsButton'> MORE ANSWERED QUESTIONS </button>
+          <button id='AddQuestionButtton'> ADD A QUESTION + </button>
+        </div>
+      </div>
+    );
   }
 
 }
