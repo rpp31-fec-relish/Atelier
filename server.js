f71c361c986df36940a35d88cf947fe12a279f13
Clayton Watterson <cwatterson@gmail.com>
2021-10-27 15:16:45 -0400

    Refactor server to handle GET, PUT and POST requests

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const token = require('./githubtoken.js');
 
 // serves the static client page
 app.use(express.static(path.join(__dirname, "client/dist")));
+app.use(express.urlencoded({extended: true}));
 
-const URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/';
+const API_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/';
 
 // accepts url and forwards to hackreactor's api, with the github token
 // as an authorization header.
@@ -15,26 +16,37 @@ const URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/';
 // https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products?count=10
 // then use
 // https://localhost:3000/api/products?count=10
-app.get('/api/*', (req, res) => {
-  console.log('Accepting GET call to API:', req.url);
-  https.get(URL + req.url.substring(5), {
+
+app.all('/api/*', (req, res) => {
+  console.log(`Accepting ${req.method} call to API:`, req.url);
+  const apiReq = https.request(new URL(API_URL + req.url.substring(5)), {
     headers: {
       Authorization: token
-    }
+    },
+    method: req.method
   }, (response) => {
+    console.log('RESPONSE');
     let body = [];
     response.on('data', (chunk) => {
       body.push(chunk);
     }).on('end', () => {
       body = Buffer.concat(body).toString();
       res.send(body);
+    }).on('error', (err) => {
+      console.error(err);
     });
   });
-});
+  if (req.method === 'POST') {
+    if (Object.keys(req.body).length != 0) {  //non empty object
+      console.log(req.body);
+      apiReq.write(JSON.stringify(req.body));
+    }
+  }
+  apiReq.on('error', (err) => {
+    console.error(err);
+  });
+  apiReq.end();
 
-app.post('/api/*', (req, res) => {
-  // todo... the only POST we will need to make to the API
-  // is adding to cart?
 });
 
 console.log('Server started on port 3000');
