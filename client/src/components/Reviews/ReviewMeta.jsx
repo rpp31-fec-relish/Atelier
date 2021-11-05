7baf8ee9021f823d56de47844d32374d0ad66fb7
MPollens2015 <maapollens@gmail.com>
2021-11-05 19:24:05 -0400

    Cleaned up ReviewMeta and Main

--- client/src/components/Reviews/ReviewMeta.jsx
+++ client/src/components/Reviews/ReviewMeta.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import helperFunctions from '../../helperFunctions';
 
-//alright, so lets set up a skelletons using the class styling for this
-
 class ReviewMeta extends React.Component {
   constructor(props){
     super(props);
@@ -12,7 +10,6 @@ class ReviewMeta extends React.Component {
       characteristics: {},
       recommended: {}
     }
-    this.button = this.button.bind(this);
   }
 
   componentDidMount() {
@@ -32,13 +29,6 @@ class ReviewMeta extends React.Component {
     })
   }
 
-  button() {
-    console.log('ratings: ', this.state.ratings)
-    console.log('characteristics: ', this.state.characteristics)
-    console.log('recommended: ', this.state.recommended)
-  }
-
-
   render() {
     return (
       <div className="reviewMeta">
@@ -50,27 +40,9 @@ class ReviewMeta extends React.Component {
         <div>False: {this.state.recommended.false}</div>
         <div> Poduct Ratings: </div>
         {Object.keys(this.state.ratings).map((rating) => <div key={rating}>{rating}: {this.state.ratings[rating]}</div>)}
-        <button onClick ={this.button}>check data</button>
       </div>
     )
   }
 }
 
 export default ReviewMeta;
-
-
-
-
-
-// const ReviewMeta = (props) => {
-//   const review = Object.keys(props.reviewMetaData.ratings).map((rating) => <div>{props.review.ratings[rating]}</div>)
-//   return (
-//   <div className="reviewMeta">
-//     <div /*---will display product rating, but again unsure where exactly we are pulling the data from, as it is not in the reviewMeta data. Also thinking of chaning this into a table, this is for proof of concept for now*/>{/*product rating*/}</div>
-//       {/* <div>{props.reviewMetaData.characteristics.Comfort.value}</div> */}
-//       {/* {Object.keys(props.reviewMetaData.ratings).map((rating) => <div>{rating}</div>)} */}
-
-//
-//   );
-// }
-
--- client/src/components/Reviews/main.jsx
+++ client/src/components/Reviews/main.jsx
@@ -49,7 +49,7 @@ class Reviews extends React.Component {
       <div id="reviews">
         <button className="reviewListSortButton" /*---turn into dropdown list that when selected will create get request with sort perameters*/>Sort reviews by relavence</button>
         <ReviewList reviewsArr={this.state.reviewsArr} /*---will need to limit rendering to two unless 'More Reviews' button is pressed, in which case it will render two more*//>
-        <ReviewMeta currentProduct={this.props.currentProduct} reviewMetaData={this.state.reviewMetaData}/*---will place to the side with CSS*//>
+        <ReviewMeta currentProduct={this.props.currentProduct} /*---will place to the side with CSS*//>
         <button className="createReviewLinkButton" /*---will link Create Review to this button which will toggle a form*/>Link to Create review</button>
         <button className="reviewListMoreReviewsButton" /*---this will toggle two more reviews to be displayed to the review list*/>More Reviews</button>
       </div>
