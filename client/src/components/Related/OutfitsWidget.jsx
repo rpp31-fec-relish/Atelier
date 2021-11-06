41f3f625fd1e9f4d3680513fa5b45c8a8f2f3177
Mary <mary.miyamoto11@gmail.com>
2021-11-06 11:06:53 -0500

    Added 'Add to Outfit' card

--- client/src/components/Related/OutfitsWidget.jsx
+++ client/src/components/Related/OutfitsWidget.jsx
@@ -4,12 +4,20 @@ import OutfitItem from './OutfitItem.jsx';
 const OutfitsWidget = (props) => {
   // go through global outfits array, get each item via helperfunction
 
+  const AddToOutfits = () => {
+    // if current product id isnt in outfits array, return 'Add to Outfits'; otherwise, return 'Remove from Outfits'
+  }
   // if (props.outfits.length === 0) {
   //   return '';
   // } else {
     return (
       <section id="OutfitsWidget">
-        <OutfitItem assignImage={props.assignImage}/>
+        <div id="OutfitItem">
+          <div id="AddToOutfit">
+            <h4>+{"\n"}Add To Outfit</h4>
+          </div>
+        </div>
+        <OutfitItem assignImage={props.assignImage} outfits={props.outfits}/>
       </section>
     )
   // }
