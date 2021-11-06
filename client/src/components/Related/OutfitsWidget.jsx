d0da5d296496d77fe22f314be4d424d2fafc11f8
Mary <mary.miyamoto11@gmail.com>
2021-11-06 13:07:12 -0500

    Continuing to work out click handling, saving to merge

--- client/src/components/Related/OutfitsWidget.jsx
+++ client/src/components/Related/OutfitsWidget.jsx
@@ -1,39 +1,42 @@
 import React from 'react';
 import OutfitItem from './OutfitItem.jsx';
 
-class OutfitsWidget extends React.Components {
+class OutfitsWidget extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      addToOutfit = true;
+      addToOutfit: true
     }
+    this.handleClick = this.handleClick.bind(this);
   }
   // go through global outfits array, get each item via helperfunction
 
-  const changeText = () => {
-    if (props.outfits.includes(props.currentProduct)) {
-      this.setState({!addToOutfit});
-    }
-  }
+  // const changeText = () => {
+  //   if (props.outfits.includes(props.currentProduct)) {
+  //     this.setState({!addToOutfit});
+  //   }
+  // }
 
-  const handleClick = (e) => {
+  handleClick(e) {
     alert('click');
   }
 
   // if (props.outfits.length === 0) {
   //   return '';
   // } else {
+
+  render() {
     return (
       <section id="OutfitsWidget">
         <div id="OutfitItem">
           <div id="AddToOutfit">
-            <h4 onClick={handleClick}>+</h4>
+            <h4 onClick={this.handleClick}>+</h4>
           </div>
         </div>
-        <OutfitItem assignImage={props.assignImage} outfits={props.outfits}/>
+        <OutfitItem assignImage={this.props.assignImage} outfits={this.props.outfits}/>
       </section>
     )
-  // }
+  }
 }
 
 export default OutfitsWidget;
