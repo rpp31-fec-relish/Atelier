1bde4605dd08cc5d760f006977085b8ed3beeb52
MPollens2015 <maapollens@gmail.com>
2021-11-06 09:16:44 -0400

    added names to divs in create, updated functionality for toggle button for recomend

--- client/src/components/Reviews/CreateReview.jsx
+++ client/src/components/Reviews/CreateReview.jsx
@@ -1,26 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import helperFunctions from '../../helperFunctions';
 
+class CreateReview extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      newReview: {
+        //this is an example, focus on the keys and change the chaecteristics portion
+        // "product_id": /* change to props.currentid*/ ,
+        // "rating": 0,
+        // "summary": "",
+        // "body": "Just the best, I live for this product",
+        recommend: false,
+        // "name": "Clayton",
+        // "email": "fakeaddy@example.com",
+        // "photos": ["", ""],
+        // "characteristics": {
+        //     "199845": 3,
+        //     "199846": 3,
+        //     "199847": 5,
+        //     "199848": 5
+        // }
+      }
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.recommend = this.recommend.bind(this);
+  }
 
-//---this will just be a basic form to fill out in order to create a new review
+  //need to create a toggle function for reccomend
 
-const CreateReview = (props) => (
-<div className="createReview">
-  <form className="createReviewForm" /*need to make a handle submit */>
-    <select className="createReviewRating" /*---need to create a select rating system of 1-5 linked to post options*/>rating</select>
-    <button className="createReviewRecomend" /*---boolean toggle for yes/no*/>Recommend?</button>
-    <th className="createReviewCharecteristics" /*---this will be a charecteristics table, will need to do a deep dive into how I want to display this, so leaving it minimalistic for now*/>
-      <tr>characteristics sample</tr>
-    </th>
-    <textarea className="createReviewSummary" /*---will  need to do a handle on change process that links to state*//>
-    <textarea className="createReviewBody" /*---will need to do a handle on change process that links to state*//>
+  recommend(e) {
+    e.preventDefault();
+    this.setState({
+      newReview: {
+      recommend: !this.state.recommend
+      }
+    })
+  }
 
-    <input className="createReviewNickname" name='nickname'></input>
-    <input className="createReviewEmail" name="email"></input>
-    <button className="createReviewSubmitButton" /*will need to do a handle submit*/>Submit</button>
-  </form>
-</div>
-);
+  handleChange(e) {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+       newReview: {
+         ...this.state.newReview, [name]:value
+       }
+    });
+  }
 
+  //need to update the create portion to include names that will add value as you type it.
+
+  render() {
+    return (
+      <div className="createReview">
+        <form className="createReviewForm" /*need to make a handle submit */>
+        <select className="createReviewRating" name='rating'/*---need to create a select rating system of 1-5 linked to post options*/>rating</select>
+        <button className="createReviewRecomend" onClick = {this.recommend}>Recommend?</button>
+        <tr className="createReviewCharecteristics" /*---this will be a charecteristics table, will need to do a deep dive into how I want to display this, so leaving it minimalistic for now*/>
+          <th>characteristics sample</th>
+        </tr>
+        <textarea className="createReviewSummary" name='summary' /*---will  need to do a handle on change process that links to state*//>
+        <textarea className="createReviewBody" name='body'/*---will need to do a handle on change process that links to state*//>
+        <input className="createReviewNickname" name='nickname'></input>
+        <input className="createReviewEmail" name="email"></input>
+        <button className="createReviewSubmitButton" /*will need to do a handle submit*/>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default CreateReview;
--- client/src/components/Reviews/main.jsx
+++ client/src/components/Reviews/main.jsx
@@ -10,9 +10,13 @@ class Reviews extends React.Component {
     super(props);
     this.state = {
       reviewsArr: [],
+      reviewCount: 2,
+      //review count will be used to show how many reviews displayed at one time, will be completed later
       reviewMetaData: {},
-      reviewCount: 2
+      displayCreateReview: false,
+      //need to figure out how the button changes to a submit button, so on submit it works
     }
+    this.handleClickOne = this.handleClickOne.bind(this);
   }
 
   componentDidMount(){
@@ -30,19 +34,34 @@ class Reviews extends React.Component {
     console.log(helperFunctions.getReviewsMetaById(currentProduct))
   }
 
+  handleClickOne() {
+    this.setState ({
+      displayCreateReview: true
+    })
+  }
+
   //---will need multiple helper functions, functions include various ajax requests (though that may be top level), switching
 
   render() {
     //---will use CSS styling for specific placement
-    return (
-      <div id="reviews">
+    let reviews;
+    if (this.state.displayCreateReview) {
+      reviews = <CreateReview currentProduct={this.props.currentProduct}/>
+    } else {
+      reviews = <div id="reviews">
         <button className="reviewListSortButton" /*---turn into dropdown list that when selected will create get request with sort perameters*/>Sort reviews by relavence</button>
         <ReviewList reviewsArr={this.state.reviewsArr} /*---will need to limit rendering to two unless 'More Reviews' button is pressed, in which case it will render two more*//>
         <ReviewMeta currentProduct={this.props.currentProduct} /*---will place to the side with CSS*//>
-        <button className="createReviewLinkButton" /*---will link Create Review to this button which will toggle a form*/>Link to Create review</button>
+        <button className="createReviewLinkButton" onClick={this.handleClickOne}>Create review</button>
         <button className="reviewListMoreReviewsButton" /*---this will toggle two more reviews to be displayed to the review list*/>More Reviews</button>
       </div>
-    );
+    }
+
+    return (
+      <div>
+        {reviews}
+      </div>
+    )
   }
 
 }
