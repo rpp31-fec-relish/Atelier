f49987d6f306988b3ef73dc9b4fef9bebeb6fe94
MPollens2015 <maapollens@gmail.com>
2021-11-06 12:54:18 -0400

    fixed drop down menu and added labels

--- client/src/components/Reviews/CreateReview.jsx
+++ client/src/components/Reviews/CreateReview.jsx
@@ -6,17 +6,18 @@ class CreateReview extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      buttonText: "Recommend?",
       newReview: {
         //this is an example, focus on the keys and change the chaecteristics portion
-        // "product_id": /* change to props.currentid*/ ,
-        // "rating": 0,
-        // "summary": "",
-        // "body": "Just the best, I live for this product",
+        product_id: props.currentProduct ,
+        rating: 0,
+        summary: "",
+        body: "",
         recommend: false,
-        // "name": "Clayton",
-        // "email": "fakeaddy@example.com",
+        name: "",
+        email: "",
         // "photos": ["", ""],
-        // "characteristics": {
+        //"characteristics": {
         //     "199845": 3,
         //     "199846": 3,
         //     "199847": 5,
@@ -24,18 +25,19 @@ class CreateReview extends React.Component {
         // }
       }
     }
-    this.handleChange = this.handleChange.bind(this);
     this.recommend = this.recommend.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-  //need to create a toggle function for reccomend
 
   recommend(e) {
     e.preventDefault();
     this.setState({
       newReview: {
       recommend: !this.state.recommend
-      }
+      },
+      buttonText: "Recommended!"
     })
   }
 
@@ -49,6 +51,19 @@ class CreateReview extends React.Component {
        }
     });
   }
+  handleChange2(e) {
+    this.setState({
+      newReview: {
+      rating: parseInt(e.target.value, 10)
+      }
+    })
+  }
+
+  //might need to put this at the main level
+
+  handleSubmit() {
+    //call helper post request using the local state as variables
+  }
 
   //need to update the create portion to include names that will add value as you type it.
 
@@ -56,16 +71,37 @@ class CreateReview extends React.Component {
     return (
       <div className="createReview">
         <form className="createReviewForm" /*need to make a handle submit */>
-        <select className="createReviewRating" name='rating'/*---need to create a select rating system of 1-5 linked to post options*/>rating</select>
-        <button className="createReviewRecomend" onClick = {this.recommend}>Recommend?</button>
+        <label>
+          Rating:
+          <select className="createReviewRating" value={this.state.value} onChange={this.handleChange2}name='rating'>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+          </select>
+        </label>
+        <button className="createReviewRecomend" onClick = {this.recommend}>{this.state.buttonText}</button>
         <tr className="createReviewCharecteristics" /*---this will be a charecteristics table, will need to do a deep dive into how I want to display this, so leaving it minimalistic for now*/>
           <th>characteristics sample</th>
         </tr>
-        <textarea className="createReviewSummary" name='summary' /*---will  need to do a handle on change process that links to state*//>
-        <textarea className="createReviewBody" name='body'/*---will need to do a handle on change process that links to state*//>
-        <input className="createReviewNickname" name='nickname'></input>
-        <input className="createReviewEmail" name="email"></input>
-        <button className="createReviewSubmitButton" /*will need to do a handle submit*/>Submit</button>
+        <label>
+          Summary:
+          <textarea className="createReviewSummary" name='summary' /*---will  need to do a handle on change process that links to state*//>
+        </label>
+        <label>
+          Body:
+          <textarea className="createReviewBody" name='body'/*---will need to do a handle on change process that links to state*//>
+        </label>
+        <label>
+          Name:
+          <input className="createReviewName" name='name'></input>
+        </label>
+        <label>
+          Email:
+          <input className="createReviewEmail" name="email"></input>
+        </label>
+        <button className="createReviewSubmitButton" type='submit'>Submit</button>
         </form>
       </div>
     );
