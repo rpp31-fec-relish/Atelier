cda368d314f3e3dbba675b77adca1c0d39cdc444
MPollens2015 <maapollens@gmail.com>
2021-11-17 18:53:49 -0500

    completed charactersitics refactoring for hooks

--- client/src/components/Reviews/CreateReview2.jsx
+++ client/src/components/Reviews/CreateReview2.jsx
@@ -5,8 +5,8 @@ import helperFunctions from '../../helperFunctions';
 function CreateReview2(props) {
 
   const[buttonText, changeButtonText] = useState('Recommended?')
-  const[newReview, ] = useState({})
-
+  const[newReview] = useState({})
+  const[characteristics, setCurrentCharacteristics] = useState({})
 
   useEffect(() => {
     console.log(props.currentProduct)
@@ -14,34 +14,44 @@ function CreateReview2(props) {
     helperFunctions.getReviewsMetaById(currentProduct)
     .then((metaData)  => {
       console.log('metaData: ', metaData);
-      let updatedProducts = {
-          product_id: props.currentProduct ,
-          rating: 0,
-          summary: "",
-          body: "",
-          recommend: false,
-          name: "",
-          email: "",
-          // "photos": ["", ""],
-          characteristics: {
-            //i need to dynamicly create the characteristics section
-          }
-
-
-        //have the object be populated with the meta data information to add to state for the charectertics, it will dynamicly fill out the state data and updata without usseing the 'setstate' function but instead an
-      }
+      console.log('productCharacteristics: ', productCharacteristics)
+      let productCharacteristics = {}
+      Object.keys(metaData.characteristics).map((key) => {
+        console.log('key: ', key);
+        productCharacteristics[key] = {
+          id: metaData.characteristics[key].id,
+          value: 0
+        }
+      })
+      setCurrentCharacteristics(productCharacteristics)
     })
     .catch((err) => {
       console.error('Error setting state of reviewMetaData', err)
     })
   }, [])
 
+  let characteristicNames = Object.keys(characteristics);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+    setCurrentCharacteristics( prevChar => {
+      return {
+      ...prevChar,
+      [name]: {value: parseInt(value, 10)}
+      }
+    })
+  }
+
+  console.log(characteristics);
 
   return (
     <div className="modalBackground">
       <div className="modalContainer">
       <button className="modalCloseButton" onClick={props.displayCreateReview}>X</button>
-        <form>
+        <form className="createReviewForm">
           <label>
             Summary:
             <textarea className="createReviewSummary" name='summary'/>
@@ -58,6 +68,19 @@ function CreateReview2(props) {
             Email:
             <input className="createReviewEmail" name="email"></input>
           </label>
+          {characteristicNames.map((trait) => <div className="charactersitic_select" key = {characteristics[trait].id}>
+            <label>
+              {trait}
+              <select value={characteristics[trait].value} onChange={handleChange}name={trait}>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+              </select>
+            </label>
+          </div>
+          )}
           <button className="createReviewSubmitButton" type='submit'>Submit</button>
         </form>
       </div>
@@ -65,6 +88,8 @@ function CreateReview2(props) {
   )
 
 }
+
+
 
 //this is a hook version of createReview
 
