ea92c975382c3ede2e680a8aae8a1c824e8505c6
Juan Acosta <jacosta091@gmail.com>
2021-11-18 20:26:24 -0700

    Added Modal for adding answer

--- client/src/components/QandA/QandAElementContainer.jsx
+++ client/src/components/QandA/QandAElementContainer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import QandAElement from './QandAElement.jsx';
 
 
-function QandAElementContainer({questions, currPageCounter, setCurrPageCounter}) {
+function QandAElementContainer({questions, currPageCounter, setCurrPageCounter, modalClick}) {
 
   const elements = questions.map((question) => {
-    return <QandAElement question={question} key={question.question_id} currPageCounter={currPageCounter} setCurrPageCounter={setCurrPageCounter}/>
+    return <QandAElement question={question} key={question.question_id} currPageCounter={currPageCounter} setCurrPageCounter={setCurrPageCounter} modalClick={modalClick}/>
   });
   if (elements.length === 0) {
     return (<div id='noQuestions'> There are no questions yet! </div>)
