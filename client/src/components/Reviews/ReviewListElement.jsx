8d6023a38d75ca0c9f4229d9e0bb5e2d386e8aa7
MPollens2015 <maapollens@gmail.com>
2021-11-27 14:53:57 -0500

    Created Recomended display

--- client/src/components/Reviews/ReviewListElement.jsx
+++ client/src/components/Reviews/ReviewListElement.jsx
@@ -2,17 +2,27 @@ import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import helperFunctions from '../../helperFunctions';
 import Stars from '../../../../node_modules/react-stars-display/';
+import CheckMark from './ReviewImages/clipart2121718_smaller.png';
 
 //---so for starters we should think about how we want to structure this. We will map over the div formation in order to show the various componenets of the review.
+
+//so we need to think about how we are going to wrap the review summary capped at 60 charecters, that are bold.
+
+//we also need to figure out how to do a "Show more" section, that if the words are longer then 250 charecters it will hide under and expansion button
+
+//we also need to allow for image submission and display. clicking a thumbnail should open the image in a modal window (thats easy, onlcick fuction will create a heref tag/link to the image in question)
 
 function ReviewListElement({review, index}) {
 
   const[wasClicked, setWasClicked] = useState(false)
+  const[wasRecommended] = useState(review.recommend)
 
   let dateTime = review.date.split('-')
 
   let months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   let timeStamp = months[parseInt(dateTime[1])-1] + ' ' + dateTime[2].slice(0,2) + ', ' + dateTime[0];
+
+  //so we can create a function that looks up the review.recommend
 
   const handleYesClick = (e) => {
     wasClicked ? null: helperFunctions.markReviewHelpfulById(review.review_id);
@@ -21,7 +31,10 @@ function ReviewListElement({review, index}) {
   const handleNoClick = (e) => {
     //need to complete
     console.log('i was clicked');
+    console.log("review:", review)
+    console.log("was recommended:", wasRecommended)
   }
+
 
 
   return(
@@ -31,9 +44,9 @@ function ReviewListElement({review, index}) {
       <div className="reviewListItemDate">{timeStamp}</div>
       <div className="reviewListItemSummary">{review.summary}</div>
       <div className="reviewListItemBody">{review.body}</div>
-      <div className="reviewListItemRecommend">{review.recommend}</div>
+      {wasRecommended ? <div className="reviewListItemRecommend">I recommend this product <img src={CheckMark} alt="CheckMark"/> </div>: ''}
       <div className="reviewListItemUser">{review.reviewer_name}</div>
-      <div className="reviewListItemResponse">{review.response}</div>
+      <div className="reviewListItemResponse">Response from seller: {review.response}</div>
       <div className="reviewListItemHelpfulness" >Was this review helpful?
       <span className="reviewListItemHelpfulnesYes" onClick={handleYesClick}> Yes {review.helpfulness}</span>
       <span onClick={handleNoClick}> No </span>
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,12 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        }
       }
     ]
   },
