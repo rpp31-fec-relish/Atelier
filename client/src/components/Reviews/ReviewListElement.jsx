368ddf9a38ab0a796ece509282897f62239354eb
MPollens2015 <maapollens@gmail.com>
2021-11-27 19:54:22 -0500

    finished charecter display function, added CSS

--- client/src/components/Reviews/ReviewListElement.jsx
+++ client/src/components/Reviews/ReviewListElement.jsx
@@ -4,11 +4,10 @@ import helperFunctions from '../../helperFunctions';
 import Stars from '../../../../node_modules/react-stars-display/';
 import CheckMark from './ReviewImages/clipart2121718_smaller.png';
 
-//---so for starters we should think about how we want to structure this. We will map over the div formation in order to show the various componenets of the review.
 
 //so we need to think about how we are going to wrap the review summary capped at 60 charecters, that are bold.
 
-//we also need to figure out how to do a "Show more" section, that if the words are longer then 250 charecters it will hide under and expansion button
+//we also need to figure out how to do a "Show more" section, that if the words are longer then 250 charecters it will hide under an expansion button
 
 //we also need to allow for image submission and display. clicking a thumbnail should open the image in a modal window (thats easy, onlcick fuction will create a heref tag/link to the image in question)
 
@@ -17,6 +16,29 @@ function ReviewListElement({review, index}) {
   const[wasClicked, setWasClicked] = useState(false)
   const[wasRecommended] = useState(review.recommend)
   const[hasResponse] = useState(review.response)
+
+  const BodyLimit = ({content, limit}) => {
+    const[showAll, setShowAll] = useState(false);
+
+    const showMore = () => setShowAll(true);
+    const showLess = () => setShowAll(false);
+
+    if (content.length <= limit) {
+      return <div className="reviewListItemBody">{content}</div>
+    }
+    if (showAll) {
+      return <div className="reviewListItemBody" >
+        {content}
+        <button className="showButton" onClick={showLess}>Read Less</button>
+      </div>
+    }
+
+    const toShow = content.substring(0, limit) + "...";
+    return <div className="reviewListItemBody" >
+      {toShow}
+      <button  className="showButton" onClick={showMore}>Read More</button>
+    </div>
+  }
 
   let dateTime = review.date.split('-')
 
@@ -27,7 +49,7 @@ function ReviewListElement({review, index}) {
     wasClicked ? null: helperFunctions.markReviewHelpfulById(review.review_id);
     setWasClicked(true);
   }
-  const handleNoClick = (e) => {
+  const handleReportClick = (e) => {
     //need to complete
     console.log('i was clicked');
     console.log("review:", review)
@@ -45,12 +67,12 @@ function ReviewListElement({review, index}) {
         <span className="reviewListItemDate">{timeStamp}</span>
       </div>
       <div className="reviewListItemSummary">{review.summary}</div>
-      <div className="reviewListItemBody">{review.body}</div>
+      <BodyLimit content={review.body} limit={250}/>
       {wasRecommended ? <div className="reviewListItemRecommend"><img src={CheckMark} alt="CheckMark"/> I recommend this product</div>: ''}
       {hasResponse ? <div className="reviewListItemResponse">Response from seller: {review.response}</div> : ''}
       <div className="reviewListItemHelpfulness">Was this review helpful?
         <span className="reviewListItemHelpfulnesYes" onClick={handleYesClick}> Yes {review.helpfulness} |</span>
-        <span onClick={handleNoClick}> Report </span>
+        <span onClick={handleReportClick}> Report </span>
       </div>
     </div>
   );
