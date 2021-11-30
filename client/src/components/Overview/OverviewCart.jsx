63bd1a0817a6a6ebc0389884222165df96e138cb
Clayton Watterson <cwatterson@gmail.com>
2021-11-30 15:00:51 -0500

    Implement Select Size

--- client/src/components/Overview/OverviewCart.jsx
+++ client/src/components/Overview/OverviewCart.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import helperFunctions from '../../helperFunctions.js';
 
 class OverviewCart extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      sku: null,
+      quantity: 1
+    }
+  }
   // Component for selections and buttons for adding to cart / favoriting
   // todo: confirm skus exist
 
@@ -11,21 +19,40 @@ class OverviewCart extends React.Component {
   }
 
   addToCartListener(event) {
-    console.log(`Added ${this.props.currentStyle.style_id}`);
+
+    if (this.state.sku != null) {
+      console.log(`Added ${this.props.currentStyle.style_id} to cart`);
+          //helperFunctions.addToCart()
+    }
+  }
+
+  selectSizeListener(event) {
+
+    if (event.target.value === 'Select Size' || event.target.value === null) {
+      return;
+    }
+    this.setState({sku: event.target.value, sizeValue: event.target.value});
+
+  }
+
+  componentDidUpdate() {
+    if (this.state.sku != null) {
+      document.getElementById('addToCartButton').removeAttribute('disabled');
+    }
   }
 
   render() {
     if (this.props.currentStyle) {
       return (
         <div>
-          <select name='size' value='Select Size' id='SizeDropdown'>
-            <option disabled>Select Size</option>
+          <select name='size' value={(this.state.sku === null) ? 'default' : this.state.sku} id='SizeDropdown' onChange={this.selectSizeListener.bind(this)}>
+            <option value='default' disabled>Select Size</option>
             {this.createSizes()}
           </select>
-          <select name='quantity' value='Select Quantity' id='QuantityDropdown'>
+          <select name='quantity' value={this.state.quantityValue} id='QuantityDropdown'>
             <option disabled>Select Quantity</option>
           </select>
-          <button onClick={this.addToCartListener.bind(this)}>Add To Cart</button>
+          <button id='addToCartButton' onClick={this.addToCartListener.bind(this)} disabled>Add To Cart</button>
           <button>Favorite</button>
         </div>
       );
